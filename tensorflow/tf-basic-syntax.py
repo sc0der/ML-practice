a2faaafe9a6b602ca7f4584be525e3ce4ae4662e
sc0der <siriusproger@gmail.com>
2021-11-21 14:46:42 +0500

    init

--- tensorflow/tf-basic-syntax.py
+++ tensorflow/tf-basic-syntax.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 import tensorflow.compat.v1 as tfc
 
-hello = tfc.constant("Hello")
-world = tfc.constant("World")
-print(hello)
 sess = tfc.InteractiveSession()
 my_tensor = tfc.random_uniform((4, 4), 0, 1)
 my_var = tfc.Variable(initial_value=my_tensor)
+print(my_var)
 result = tfc.global_variables_initializer()
-print(sess.run(result))
+sess.run(result)
+sess.run(my_var)
+# print(ddd)
