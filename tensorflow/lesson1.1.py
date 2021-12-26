33451534c014e1d7ead58a0ad08d717cbcae981b
Sirius Sufiew <siriusproger@gmail.com>
2021-12-26 17:15:33 +0500

    added Example: normalizing features

--- tensorflow/lesson1.1.py
+++ tensorflow/lesson1.1.py
@@ -1,1 +1,15 @@
 # Example: normalizing features
+
+import numpy as np
+
+from tensorflow.keras.layers import Normalization
+
+# Example image data, with values in the [0, 255] range
+training_data = np.random.randint(0, 256, size=(64, 200, 200, 3)).astype("float32")
+
+normalizer = Normalization(axis=-1)
+normalizer.adapt(training_data)
+
+normalized_data = normalizer(training_data)
+print("var: %.4f" % np.var(normalized_data))
+print("mean: %.4f" % np.mean(normalized_data))
